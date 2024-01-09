deba51e04fecc6fab739ec3a06038f408a5d8a81
Israr Khan <78822488+isrark005@users.noreply.github.com>
2024-01-09 14:27:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
                 },
                 ...prev.slice(0, 9)
             ]);
-        }, 500);
+        }, 1000 * 60 * 60 * 24);
     
        
         return () => clearInterval(intervalId);
